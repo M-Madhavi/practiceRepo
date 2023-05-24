860afa63bb126e2fafecf8423534a8c925d632c1
M-Madhavi <madhavinaidu424@gmail.com>
2023-05-24 16:33:14 +0530

    files format

--- Node/app.js
+++ Node/app.js
@@ -5,21 +5,17 @@ var cors = require('cors')
 const connection = require('./connection')
 const app = express()
 const userRoute = require("./routes/user")
-const port =process.env.PORT
+const port = process.env.PORT
 app.use(cors());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.get('/', function (req, res) {
-    res.send('Hello World!'); 
-  });
-  
+  res.send('Hello World!');
+});
+
 app.use('/user', userRoute)
 
-
-
-// const server = http.createServer(app)
-// server.listen(()=>{
-app.listen(port,() => {
-    console.log("App listening", port
-    )
+app.listen(port, () => {
+  console.log("App listening", port
+  )
 })
--- Node/connection.js
+++ Node/connection.js
@@ -2,19 +2,18 @@ require('dotenv')
 const mysql = require('mysql')
 
 var connection = mysql.createConnection({
- port:process.env.DB_PORT,
- host:process.env.DB_HOST,
- user:process.env.DB_USERNAME,
- password:process.env.DB_PASSWORD,
- database:process.env.DB_NAME   
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 })
-connection.connect((err) =>{
-    if(!err){
+connection.connect((err) => {
+    if (!err) {
         console.log("Connected to DB")
-    }else{
+    } else {
         console.log("Failed Connected to DB")
-
     }
 })
-module.exports=connection
+module.exports = connection
 
