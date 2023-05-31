98856eb5ec3a6b02eed9fc259268afb6b8cf7458
M-Madhavi <madhavinaidu424@gmail.com>
2023-05-31 18:15:23 +0530

    bill generator - pdf format

--- Node/routes/bill.js
+++ Node/routes/bill.js
@@ -1,42 +1,57 @@
-const express = require('express')
-const connection = require('../connection')
-const router = express.Router()
-let ejs = require('ejs')
-let pdf = require('html-pdf')
-let path = require('path')
-const fs = require('fs')
-const uuid = require('uuid')
-const auth = require('../services/authentication')
+const express = require('express');
+const connection = require('../connection');
+const router = express.Router();
+const ejs = require('ejs');
+const path = require('path');
+const fs = require('fs');
+const uuid = require('uuid');
+const auth = require('../services/authentication');
+// const PDFDocument = require('pdfkit');
+const pdf = require('html-pdf');
+const rootFolderPath = path.join(__dirname, '../generatedpdf');
 
-
-router.post('/generatereport', auth.authenticationToken, (req, res) => {
+const templatePath = path.resolve(__dirname, './report.ejs');
+const puppeteer = require('puppeteer');
+const { PDFDocument } = require('pdf-lib');
+router.post('/generatereport', auth.authenticationToken, async (req, res) => {
     const generateuuid = uuid.v1();
     const orderDetails = req.body;
     const productDetailsReport = JSON.parse(orderDetails.productDetails);
-    console.log("productDetailsReport",productDetailsReport,generateuuid,orderDetails)
-    let query = "insert into bill (name,uuid,email,contactNumber,paymentMethod,total,productDetails,createdBy) values(?,?,?,?,?,?,?,?)"
-    connection.query(query, [orderDetails.name, generateuuid, orderDetails.email, orderDetails.contactNumber, orderDetails.paymentMethod, orderDetails.totalAmount, orderDetails.productDetails, res.locals.email], (err, resp) => {
-        if (!err) {
-            ejs.renderFile(path.join(__dirname, '', "report.ejs"), { productDetails: productDetailsReport, name: orderDetails.name, email: orderDetails.email, contactNumber: orderDetails.contactNumber, paymentMethod: orderDetails.paymentMethod, totalAmount: orderDetails.totalAmount }, (err, results) => {
-                if (err) {
-                    return res.status(500).json(err)
-                }
-                else {
-                    pdf.create(results).toFile('./generated_pdf/' + generateuuid + ".pdf", function (err, data) {
-                        if (err) {
-                            console.log("@@@@@@errr", err)
-                            return res.status(500).json(err)
-                        } else {
-                            return res.status(200).json({ uuid: generateuuid })
-                        }
-                    })
-                }
-            })
-        }
-        else {
-            return res.status(500).json(err)
-        }
-    })
-})
+    let data = {
+        name: orderDetails.name,
+        email: orderDetails.email,
+        contactNumber: orderDetails.contactNumber,
+        paymentMethod: orderDetails.paymentMethod,
+        totalAmount: orderDetails.totalAmount,
+        productDetails: productDetailsReport,
+    };
 
-module.exports = router
+    console.log("productDetailsReport", productDetailsReport, generateuuid, orderDetails);
+
+    let query = "insert into bill (name,uuid,email,contactNumber,paymentMethod,total,productDetails,createdBy) values(?,?,?,?,?,?,?,?)";
+    connection.query(query, [orderDetails.name, generateuuid, orderDetails.email, orderDetails.contactNumber, orderDetails.paymentMethod, orderDetails.totalAmount, orderDetails.productDetails, res.locals.email], async (err, resp) => {
+        if (!err) {
+            const html = await ejs.renderFile(templatePath, data);
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.setContent(html);
+
+            const pdfBytes = await page.pdf({ format: 'A4' });
+            await browser.close();
+
+            const outputPath = path.join(rootFolderPath, `./${generateuuid}.pdf`);
+            fs.writeFileSync(outputPath, pdfBytes);
+
+            console.log('PDF generated successfully');
+            return res.status(200).json({ uuid: generateuuid });
+        } else {
+            console.error('Error generating PDF:', err);
+            return res.status(500).json({ error: err });
+        }
+    });
+});
+
+module.exports = router;
+
+
+
